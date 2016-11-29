0188fa21ee2168eb2980974c0b89d0f906a410f3
Simon Rabourg <simon.rabourg@ensimag.grenoble-inp.fr>
2016-11-28 23:19:33 -0300

    Initializing the project with parsing of movies, tags and links implemented

---
+++ src/DBParser.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class DBParser {
+	
+	private final static String RESOURCES_PATH = "./ml-20m/";
+	private final static String MOVIES_FILE_NAME = "movies.csv";
+	private final static String LINKS_FILE_NAME = "links.csv";
+	private final static String TAGS_FILE_NAME = "genome-tags.csv";
+	private final static String SEPARATOR = ",";
+	private HashMap <Integer, String> movies;
+	private HashMap <Integer, Integer> imdbId;
+	private HashMap <Integer, Integer> wikidataId;
+	private HashMap <Integer, String> tags;
+	
+	public DBParser() {
+		movies = new HashMap<Integer, String>();
+		imdbId = new HashMap<Integer, Integer>();
+		wikidataId = new HashMap<Integer, Integer>();
+		tags = new HashMap<Integer, String>();
+	}
+	
+    public void parseMovies() throws IOException {
+        InputStream fr = new FileInputStream(RESOURCES_PATH + MOVIES_FILE_NAME);
+        
+        InputStreamReader ipsr = new InputStreamReader(fr);
+		BufferedReader br = new BufferedReader(ipsr);
+		String ligne;
+		// Consume the first line
+		br.readLine();
+		while ((ligne=br.readLine()) != null){
+			movies.put(Integer.parseInt(ligne.split(SEPARATOR)[0]), ligne.split(SEPARATOR)[1]);
+			System.out.println(movies.get(Integer.parseInt(ligne.split(SEPARATOR)[0])));
+		}
+		br.close(); 
+        
+    }
+    
+    public void parseLinks() throws IOException {
+        InputStream fr = new FileInputStream(RESOURCES_PATH + LINKS_FILE_NAME);
+        
+        InputStreamReader ipsr = new InputStreamReader(fr);
+		BufferedReader br = new BufferedReader(ipsr);
+		String ligne;
+		// Consume the first line
+		br.readLine();
+		while ((ligne=br.readLine()) != null){
+			if (ligne.split(SEPARATOR).length >= 2) {
+				imdbId.put(Integer.parseInt(ligne.split(SEPARATOR)[0]), Integer.parseInt(ligne.split(SEPARATOR)[1]));
+				System.out.println(imdbId.get(Integer.parseInt(ligne.split(SEPARATOR)[0])));
+			}
+			if (ligne.split(SEPARATOR).length >= 3) {
+				wikidataId.put(Integer.parseInt(ligne.split(SEPARATOR)[0]), Integer.parseInt(ligne.split(SEPARATOR)[2]));
+				System.out.println(wikidataId.get(Integer.parseInt(ligne.split(SEPARATOR)[0])));
+			}
+		}
+		br.close(); 
+        
+    }
+    
+    public void parseTags() throws IOException {
+        InputStream fr = new FileInputStream(RESOURCES_PATH + TAGS_FILE_NAME);
+        
+        InputStreamReader ipsr = new InputStreamReader(fr);
+		BufferedReader br = new BufferedReader(ipsr);
+		String ligne;
+		// Consume the first line
+		br.readLine();
+		while ((ligne=br.readLine()) != null){
+			tags.put(Integer.parseInt(ligne.split(SEPARATOR)[0]), ligne.split(SEPARATOR)[1]);
+			System.out.println(tags.get(Integer.parseInt(ligne.split(SEPARATOR)[0])));
+		}
+		br.close(); 
+        
+    }
+    
+    public static void main(String args[]){
+    	DBParser dbp = new DBParser();
+    	try {
+			dbp.parseMovies();
+			dbp.parseLinks();
+			dbp.parseTags();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
